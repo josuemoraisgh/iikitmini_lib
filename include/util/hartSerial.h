e97e7d94addb258600b8ee0c45f0c9635fb355e6
alunoUFU <alunoufu@ufu.br>
2025-02-13 06:59:57 -0300

    refactor hartSerial.h

---
+++ include/util/hartSerial.h
@@ -0,0 +1,30 @@
+#ifndef HART_SERIAL_H
+#define HART_SERIAL_H
+
+#include <Arduino.h>
+
+class HartSerial {
+public:
+    HartSerial(uint32_t baudRate = 115200) {
+        this->baudRate = baudRate;
+    }
+
+    void begin() {
+        Serial.begin(baudRate);
+        Serial2.begin(baudRate, SERIAL_8N1, 16, 17);
+    }
+
+    void update() {
+        while (Serial.available()) {
+            Serial2.write(Serial.read());
+        }
+        while (Serial2.available()) {
+            Serial.write(Serial2.read());
+        }
+    }
+
+private:
+    uint32_t baudRate;
+};
+
+#endif
